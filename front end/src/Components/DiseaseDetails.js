4ac2398bd1855d6075207b38d6ea6c3fed8c454b
Kavinigeethasinghe <99945370+Kavinigeethasinghe@users.noreply.github.com>
2023-04-09 14:47:20 +0530

    Update DiseaseDetails.js

--- front end/src/Components/DiseaseDetails.js
+++ front end/src/Components/DiseaseDetails.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import "./DiseaseDetails.css"; 
+import React, { useState } from 'react'
+import './DiseaseDetails.css'
 
 function Disease() {
 
-  const [selectedDisease, setSelectedDisease] = useState(null);
+  const [selectedDisease, setSelectedDisease] = useState(null)
   
   const diseases = [
     {
@@ -13,7 +13,7 @@ function Disease() {
       image2: 'alopecia_areata_s1.jpg',
       image3: 'alopecia_areata_s2.jpg',
       image4: 'alopecia_areata_s3.jpg',
-      selfTreatment: 'Mix honey and a few drops of lemon and apply it on your scalp.Make a Fenugreek seeds paste and apply it on your scalp and leave it overnight.Squeeze onion juice and apply it on your scalp. Mix garlic juice and coconut oil, simmer in low flame and aplly it on your scalp. ',
+      selfTreatment: 'Mix honey and a few drops of lemon and apply it on your scalp.Make a Fenugreek seeds paste and apply it on your scalp and leave it overnight.Squeeze onion juice and apply it on your scalp. Mix garlic juice and coconut oil, simmer in low flame and aplly it on your scalp. '
     },
     {
       name: 'Contact Dermatitis',
@@ -22,7 +22,7 @@ function Disease() {
       image2: 'contact_dermatitis_s1.jpg',
       image3: 'contact_dermatitis_s2.jpg',
       image4: 'contact_dermatitis_s3.jpg',
-      selfTreatment: '1.Avoid scratching your irritated skin.Clean your skin with mild soap and lukewarm water to remove any irritants.Apply petroleum jelly like Vaseline to soothe the area. Apply anti-itch treatments such as calamine lotion or hydrocortisone cream.',
+      selfTreatment: '1.Avoid scratching your irritated skin.Clean your skin with mild soap and lukewarm water to remove any irritants.Apply petroleum jelly like Vaseline to soothe the area. Apply anti-itch treatments such as calamine lotion or hydrocortisone cream.'
     },
     {
       name: 'Folliculitis',
@@ -31,7 +31,7 @@ function Disease() {
       image2: 'folliculitis_s1.jpg',
       image3: 'folliculitis_s2.jpg',
       image4: 'folliculitis_s3.jpg',
-      selfTreatment: '1.Apply warm, damp cloth to your scalp a few times a day. Wash your scalp with an anti-dandruff shampoo. Apply Cortisone cream or Antibiotic ointment. Wash your hair with anti-bacterial soap.',
+      selfTreatment: '1.Apply warm, damp cloth to your scalp a few times a day. Wash your scalp with an anti-dandruff shampoo. Apply Cortisone cream or Antibiotic ointment. Wash your hair with anti-bacterial soap.'
     },
     {
       name: 'Head Lice',
@@ -40,7 +40,7 @@ function Disease() {
       image2: 'head_lice_s1.jpg',
       image3: 'head_lice_s2.jpg',
       image4: 'head_lice_s3.jpg',
-      selfTreatment: 'Wash your hair daily. Wet-combing your hair using a fine-toothed comb. Apply essential oil and leave it overnight. Mix garlic and few drops of lime juice and then apply the paste on your scalp.',
+      selfTreatment: 'Wash your hair daily. Wet-combing your hair using a fine-toothed comb. Apply essential oil and leave it overnight. Mix garlic and few drops of lime juice and then apply the paste on your scalp.'
     },
     {
       name: 'Lichen Planus',
@@ -49,16 +49,16 @@ function Disease() {
       image2: 'lichen_planus_s1.jpg',
       image3: 'lichen_planus_s2.jpg',
       image4: 'lichen_planus_s3.jpg',
-      selfTreatment: ' Soak in an oatmeal bath. Avoid scratching. Apply cool compresses to a rash. Use OTC anti-itch creams.',
+      selfTreatment: ' Soak in an oatmeal bath. Avoid scratching. Apply cool compresses to a rash. Use OTC anti-itch creams.'
     },
     {
       name: 'Male pattern baldness',
-      description: 'Male pattern baldness is hair loss and thinning of the hair that affects the hairline and top of the head. It is a very common condition for men over 40 years of age.',
+      description: 'Male pattern baldness is hair loss and thinning of the hair that affects the hairline and top of the head. It is a very common condition for men over 40 years of age.'
       image1: 'male_pattern_baldness_l.jpg',
       image2: 'male_pattern_baldness_s1.jpg',
       image3: 'male_pattern_baldness_s2.jpg',
       image4: 'male_pattern_baldness_s3.jpg',
-      selfTreatment: 'Apply Garlic, onion, ginger juice on your scalp. Apply a paste of a few drops of Apple Cider Vinegar with water on your scalp. Applying Essential oils on your scalp. ',
+      selfTreatment: 'Apply Garlic, onion, ginger juice on your scalp. Apply a paste of a few drops of Apple Cider Vinegar with water on your scalp. Applying Essential oils on your scalp. '
     },
     {
       name: 'Psoriasis',
@@ -67,7 +67,7 @@ function Disease() {
       image2: 'psoriasis_s1.jpg ',
       image3: 'psoriasis_s2.jpg',
       image4: 'psoriasis_s3.jpg',
-      selfTreatment: '1.Apply creams that contain 0.5% aloe vera. Apply a mixture of baking soda and water on the affected area. Having an Oatmeal bath. Use shampoo that contain 2% to 10% coal tar or anti-inflammatory salicylic acid.',
+      selfTreatment: '1.Apply creams that contain 0.5% aloe vera. Apply a mixture of baking soda and water on the affected area. Having an Oatmeal bath. Use shampoo that contain 2% to 10% coal tar or anti-inflammatory salicylic acid.'
     },
     {
       name: 'Seborrheic dermatitis',
@@ -76,7 +76,7 @@ function Disease() {
       image2: 'seborrheic_dermatitis_s1.jpg',
       image3: 'seborrheic_dermatitis_s2.jpg',
       image4: 'seborrheic_dermatitis_s3.jpg',
-      selfTreatment: 'Cant give self treatment methods. Please contact Dematologist',
+      selfTreatment: 'Cant give self treatment methods. Please contact Dematologist'
     },
     {
       name: 'Telogen Effluvium ',
@@ -85,7 +85,7 @@ function Disease() {
       image2: 'telogen_effluvium_s1.jpg ',
       image3: 'telogen_effluvium_s2.jpg ',
       image4: 'telogen_effluvium_s3.jpg ',
-      selfTreatment: 'Cant give self treatment methods. Please contact Dematologist',
+      selfTreatment: 'Cant give self treatment methods. Please contact Dematologist'
     },
     {
       name: 'Tinea capitis',
@@ -94,9 +94,9 @@ function Disease() {
       image2: 'tinea_capitis_s1.jpg',
       image3: 'tinea_capitis_s2.jpg',
       image4: 'tinea_capitis_s3.jpg',
-      selfTreatment: 'Dab with Tea Tree Oil. Rinse with Apple Cider Vinegar.',
+      selfTreatment: 'Dab with Tea Tree Oil. Rinse with Apple Cider Vinegar.'
     }
-  ];
+  ]
 
   // const diseaseIndex = 1;
   // const handleDiseaseSelect = (diseaseIndex) => {
@@ -104,16 +104,16 @@ function Disease() {
   // };
 
   const handleDiseaseSelect = () => {
-    setSelectedDisease(diseases[1]);
-  };
+    setSelectedDisease(diseases[1])
+  }
 
   return (
     <div>
-      <button onClick={handleDiseaseSelect} className="ghost">
+      <button onClick={handleDiseaseSelect} className='ghost'>
         '''Successfully Predicted the Disease 
         Click Here to view details!!'''
-       </button>
-    {/* { <div className="disease-list">
+      </button>
+      {/* { <div className="disease-list">
       {diseases.map((disease, index) => (
         <div key={index} className="disease" onClick={() => handleDiseaseSelect(index)}>
           <h2>{disease.name}</h2>
@@ -121,20 +121,19 @@ function Disease() {
         </div>
       ))}
     </div> } */}
-    {selectedDisease &&   (
-      <div className="details">
-        <div className="image-card1"><img src={selectedDisease.image1} alt="image1" /></div>
-        <div className="image-card2"><img src={selectedDisease.image2} alt="image2" /></div>
-        <div className="image-card3"><img src={selectedDisease.image3} alt="image3" /></div>
-        <div className="image-card4"><img src={selectedDisease.image4} alt="image4" /></div>
-        <div className="details-card1"><h2>{selectedDisease.name}</h2></div>
-        <div className="details-card2"><p>{selectedDisease.description}</p></div>
-        <div className="details-card3"><p>{selectedDisease.selfTreatment}</p></div>
+       {selectedDisease && (
+      <div className= 'details'>
+        <div className='image-card1'><img src={selectedDisease.image1} alt='image1' /></div>
+        <div className='image-card2'><img src={selectedDisease.image2} alt='image2' /></div>
+        <div className='image-card3'><img src={selectedDisease.image3} alt='image3' /></div>
+        <div className='image-card4'><img src={selectedDisease.image4} alt='image4' /></div>
+        <div className='details-card1'><h2>{selectedDisease.name}</h2></div>
+        <div className='details-card2'><p>{selectedDisease.description}</p></div>
+        <div className='details-card3'><p>{selectedDisease.selfTreatment}</p></div>
       </div>
     )}
-  </div>
-    
-  );
+    </div>
+  )
 }
 
-export default Disease;
+export default Disease
